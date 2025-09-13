2f2ac76caf40ec424b7aef5b9cc9c0c1ca9d7e13
Arya <aryapanwar493@gmail.com>
2025-09-13 16:39:18 +0530

    Small fix

--- src/components/custom/reloadModel.jsx
+++ src/components/custom/reloadModel.jsx
@@ -33,6 +33,7 @@ export function ReloadModal({ captchaImage, captcha, setCaptcha, handleLogin, me
                             <button
                                 type="submit"
                                 className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition hover:cursor-pointer"
+                                onClick={handleLogin}
                             >
                                 Submit
                             </button>
