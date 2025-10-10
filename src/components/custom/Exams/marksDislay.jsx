625f49c7a13f20609c6d61db48bcbdf20ba80914
Arya <85472696+Arya4930@users.noreply.github.com>
2025-10-11 00:39:29 +0530

    Fix

--- src/components/custom/Exams/marksDislay.jsx
+++ src/components/custom/Exams/marksDislay.jsx
@@ -81,7 +81,7 @@ export default function MarksDisplay({ data }) {
 
               {/* Full page modal */}
               {openCourse === course.slNo && (
-                <div className="fixed inset-0 flex items-center justify-center bg-black/40 backdrop-blur-sm z-50">
+                <div data-scrollable className="fixed inset-0 flex items-center justify-center bg-black/40 backdrop-blur-sm z-50">
                   <div className="bg-white dark:bg-slate-800 midnight:bg-black rounded-xl shadow-lg p-6 max-w-3xl w-[95%] relative max-h-[90vh] overflow-y-auto">
                     <h2 className="text-lg font-semibold mb-4 text-gray-900 dark:text-gray-100 midnight:text-gray-100">
                       {course.courseCode} – {course.courseTitle}
