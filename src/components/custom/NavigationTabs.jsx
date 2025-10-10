2699c1bafb754f4908b4e58e3430fe747b4bf222
Arya <aryapanwar493@gmail.com>
2025-10-10 15:52:58 +0530

    Testing fixes

--- src/components/custom/NavigationTabs.jsx
+++ src/components/custom/NavigationTabs.jsx
@@ -23,7 +23,7 @@ export default function NavigationTabs({
     "bg-gray-200 text-gray-700 hover:bg-gray-300 dark:bg-slate-800 dark:text-gray-300 dark:hover:bg-slate-700 midnight:bg-black midnight:text-gray-100 midnight:hover:bg-gray-800";
 
   return (
-    <div className="flex w-full shadow-sm pb-4 dark:bg-slate-900 midnight:bg-black">
+    <div data-scrollable className="flex w-full shadow-sm pb-4 dark:bg-slate-900 midnight:bg-black">
       <button
         onClick={handleLogOutRequest}
         className="w-12 flex items-center justify-center bg-red-500 hover:cursor-pointer text-white text-sm font-medium hover:bg-red-600 dark:bg-red-600 dark:hover:bg-red-700 midnight:bg-red-700 midnight:hover:bg-red-800 transition-colors"
