06187412c2c0fefee0fac63701237fb6de23ef75
Arya <aryapanwar493@gmail.com>
2025-12-08 23:26:20 +0530

    Removing old crypto lib

--- api/src/mask.ts
+++ api/src/mask.ts
@@ -1,11 +1,10 @@
-import crypto from "crypto";
+import { createHmac } from "node:crypto";
 import "dotenv/config";
 
 const SALT = process.env.ID_SALT!;
 
 export function maskUserID(userID: string): string {
-  return crypto
-    .createHmac("sha256", SALT)
+  return createHmac("sha256", SALT)
     .update(userID)
     .digest("hex")
     .substring(0, 16);
