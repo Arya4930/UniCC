1faa033968fc7e5d9c2189732eb57e9c57bbb5bd
Arya <85472696+Arya4930@users.noreply.github.com>
2026-09-19 12:38:40 +0530

    Comment out API issue notification div
    Commented out a div displaying a message about API issues after a VTOP update.

--- src/components/custom/Main.tsx
+++ src/components/custom/Main.tsx
@@ -612,9 +612,9 @@ export default function LoginPage() {
 
       {(isLoggedIn || demoMode) && (
         <>
-        <div className="top-0 left-0 w-full bg-yellow-500 text-black text-center py-2 font-medium">
+            {/* <div className="top-0 left-0 w-full bg-yellow-500 text-black text-center py-2 font-medium">
         After the recent VTOP update our API seems to be not working properly. We are working on fixing it. Please check back later. ⚠️
-      </div>
+      </div> */}
           {isOffline && <div className="top-0 left-0 w-full bg-yellow-500 text-black text-center py-2 font-medium">
             ⚠️ You’re currently offline. Some features may not work.
           </div>}
